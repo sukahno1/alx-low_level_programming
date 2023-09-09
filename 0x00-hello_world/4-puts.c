2b429ea1c13a1b49a527367606cc801348f1bb54
sukahno1 <sukakinwi@gmail.com>
2023-09-09 10:20:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+      puts("Programming is like building a multilingual puzzle\n");
+      return (0);
+}
