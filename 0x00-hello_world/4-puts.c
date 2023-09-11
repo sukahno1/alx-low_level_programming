ff15b58055cd2d0edb96d347ecab0e5431a4386c
sukahno1 <sukakinwi@gmail.com>
2023-09-10 20:35:11 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Point of entry
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
